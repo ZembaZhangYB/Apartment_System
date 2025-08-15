41018feaae759b1d49d918811e98920f96cd21ab
zyb <541742997@qq.com>
2025-08-15 16:53:54 +0800

    Initial commit

---
+++ model/src/main/java/com/atguigu/model/service/ApartmentFacilityService.java
@@ -0,0 +1,14 @@
+package com.atguigu.model.service;
+
+
+import com.atguigu.model.domain.ApartmentFacility;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author HP
+* @description 针对表【apartment_facility(公寓&配套关联表)】的数据库操作Service
+* @createDate 2025-08-15 16:50:14
+*/
+public interface ApartmentFacilityService extends IService<ApartmentFacility> {
+
+}
